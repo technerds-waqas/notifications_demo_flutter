b87732bc965664bf4eda61a5baec5a9000414756
Waqas Shafi <waqasshafi@technerds.com>
2022-12-08 14:59:28 +0500

    added new code for local notification plugin

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'main.dart';
 
@@ -20,6 +21,12 @@ class _HomePageState extends State<HomePage> {
               '\nTitle=${message.notification?.title},'
               '\nBody=${message.notification?.body},'
               '\nData=${message.data}';
+          // Get.showSnackbar(GetSnackBar(
+          //   title: message.notification?.title,
+          //   message: message.notification?.body,
+          //   backgroundColor: Colors.amber,
+          //   duration: const Duration(microseconds: 4000),
+          // ));
         } else {
           _lastMessage = 'Received a data message: ${message.data}';
         }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:developer';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
 
 import 'firebase_options.dart';
 // FlutterFire's Firebase Cloud Messaging plugin
@@ -20,12 +22,26 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
 
   if (kDebugMode) {
-    print("Handling a background message: ${message.messageId}");
-    print('Message data: ${message.data}');
-    print('Message notification: ${message.notification?.title}');
-    print('Message notification: ${message.notification?.body}');
+    log("Handling a background message: ${message.messageId}");
+    log('Message data: ${message.data}');
+    log('Message notification: ${message.notification?.title}');
+    log('Message notification: ${message.notification?.body}');
   }
 }
+
+// Local notifcation plugin object
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+// Android notification channel from local notification plugin
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description: 'This channel is used for important notifications.',
+    // description
+    importance: Importance.max,
+    playSound: true);
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -57,22 +73,68 @@ Future<void> main() async {
   if (kDebugMode) {
     log('Registration Token=$token');
   }
-  // TODO: Set up foreground message handler
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    if (kDebugMode) {
-      print('Handling a foreground message: ${message.messageId}');
-      print('Message data: ${message.data}');
-      print('Message notification: ${message.notification?.title}');
-      print('Message notification: ${message.notification?.body}');
-    }
 
+  // from local notification plugin
+  await messaging.setForegroundNotificationPresentationOptions(
+      alert: true, badge: true, sound: true);
+
+  // TODO: Set up foreground message handler
+
+  // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  //   if (kDebugMode) {
+  //     print('Handling a foreground message: ${message.messageId}');
+  //     print('Message data: ${message.data}');
+  //     print('Message notification: ${message.notification?.title}');
+  //     print('Message notification: ${message.notification?.body}');
+  //   }
+
+  //   messageStreamController.sink.add(message);
+  // });
+
+  //replaced by another code for handling foreground notification display
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    RemoteNotification? notification = message.notification;
+    AndroidNotification? android = message.notification?.android;
+
+    log('Got a message whilst in the foreground!');
+    log('Message data: ${message.data}');
     messageStreamController.sink.add(message);
+    // if (message.notification != null) {
+    //   print('Message also contained a notification: ${message.notification}');
+    // }
+    // If `onMessage` is triggered with a notification, construct our own
+    // local notification to show to users using the created channel.
+    if (notification != null && android != null) {
+      flutterLocalNotificationsPlugin.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            channel.id,
+            channel.name,
+            channelDescription: channel.description,
+            playSound: true,
+            color: Colors.blue,
+            icon: '@mipmap/launcher_icon',
+
+            // other properties...
+          ),
+        ),
+      );
+    }
   });
+
+  // from local notification plugin
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
 
   // TODO: Set up background message handler
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -80,7 +142,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
